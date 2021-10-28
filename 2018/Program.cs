807c1bd11a14e58b4e9bd2bf761e77ea771233de
Ben <bbevan196@gmail.com>
2021-10-28 19:57:00 +0100

    start 2018

---
+++ 2018/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _2018
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(RepeatFrequency(@"data\1.txt"));
+        }
+
+		private static int RepeatFrequency(string path)
+		{
+			var freq = 0;
+			var lines = File.ReadAllLines(path).Select(int.Parse).ToArray();
+			var f = new List<int>(new[] {0});
+
+			for (var i = 0; i <= lines.Length; i++)
+			{
+				if (i == lines.Length) 
+				{
+					i = -1;
+					continue;
+				}
+
+				freq += lines[i];
+				if (!f.Contains(freq))
+				{
+					f.Add(freq);
+				}
+				else
+				{
+					return freq;
+				}
+			}
+			return 0;
+		}
+
+		
+    }
+}

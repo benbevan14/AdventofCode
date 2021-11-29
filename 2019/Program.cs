02a7a2c47fa156311561b15cb72bb975b1d311cb
benbevan14 <b.bevan@ucas.ac.uk>
2021-11-29 09:48:23 +0000

    2019-10

--- 2019/Program.cs
+++ 2019/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.IO;
 
 namespace _2019
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(BestAsteroid(@"data/test.txt"));
         }
+
+		private static int BestAsteroid(string path)
+		{
+			var field = ReadGrid(path);
+
+			DisplayGrid(field);
+
+			
+
+			return 0;
+		}
+
+		private static char[,] ReadGrid(string path)
+		{
+			var input = File.ReadAllLines(path);
+			var size = input[0].Length;
+			var grid = new char[size, size];
+
+			for (var col = 0; col < size; col++)
+			{
+				for (var row = 0; row < size; row++)
+				{
+					grid[col, row] = input[row][col];
+				}
+			}
+
+			return grid;
+		}
+
+		private static void DisplayGrid(char[,] grid)
+		{
+			for (var col = 0; col < grid.GetLength(0); col++)
+			{
+				for (var row = 0; row < grid.GetLength(1); row++)
+				{
+					Console.Write(grid[row, col] + " ");
+				}
+				Console.WriteLine();
+			}
+		}
     }
 }

7127a174b1b1906a1876f6aa5cd80256ece678f6
benbevan14 <b.bevan@ucas.ac.uk>
2021-12-01 09:16:08 +0000

    2021-1

--- 2021/Program.cs
+++ 2021/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace _2021
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(SlidingDepth(@"data/1.txt"));
         }
+
+		private static int IncreasingDepth(string path)
+		{
+			var count = 0;
+			var depths = File.ReadAllLines(path).Select(int.Parse).ToArray();
+			for (var i = 0; i < depths.Length - 1; i++)
+			{
+				if (depths[i + 1] > depths[i]) count++;
+			}
+
+			return count;
+		}
+
+		private static int SlidingDepth(string path)
+		{
+			var count = 0;
+			var depths = File.ReadAllLines(path).Select(int.Parse).ToArray();
+			for (var i = 0; i < depths.Length - 3; i++)
+			{
+				if (depths[i + 3] > depths[i]) count++;
+			}
+
+			return count;
+		}
     }
 }
